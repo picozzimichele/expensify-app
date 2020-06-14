a5653bb3234e2860d2bc2ad4033940b9d8591886
michele picozzi <michelepicozzi@micheles-MacBook-Pro.local>
2020-06-14 20:44:56 +0900

    setup dev dependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
-  const isProduction = env === "production"
-  const CSSExtract = new ExtractTextPlugin("styles.css")
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: {
@@ -21,13 +21,13 @@ module.exports = (env) => {
         use: CSSExtract.extract({
           use: [
             {
-              loader: "css-loader",
+              loader: 'css-loader',
               options: {
                 sourceMap: true
               }
             },
             {
-              loader: "sass-loader",
+              loader: 'sass-loader',
               options: {
                 sourceMap: true
               }
@@ -39,10 +39,11 @@ module.exports = (env) => {
     plugins: [
       CSSExtract
     ],
-    devtool: isProduction ? "source-map" : 'inline-source-map',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
-  }
+  };
 };
